64fcbd74e55172ea2fe6643c10d2bf4c42562730
Austin <acw0415@gmail.com>
2022-09-27 15:22:28 -0400

    backend and style changes

---
+++ client/src/styled-comps/AboutStyle.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+import theme from "./theme"
+
+const AboutStyle = styled.div`
+
+    background-color: ${theme.darkGray};
+
+
+`
+
+export default AboutStyle
